e27b60d7bebee7e93c3ffe805d7b249b4ec2dd1d
Michelle Moon <michellemoon236@gmail.com>
2019-04-25 18:17:53 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author 
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts 
+    @posts 
+  end 
+  
+  def add_post(one_post)
+    @posts << one_post
+    one_post.author = self 
+  end 
+  
+  def add_post_by_title(post_title)
+    one_post = Post.new(post_title)
+    @posts << one_post
+    one_post.author = self 
+  end 
+  
+  def self.post_count
+    Post.all.count 
+  end
+end 
